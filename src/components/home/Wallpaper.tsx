73aaa00706454686a134784c32bc9fdfb9e5f00e
Steven Tete <128250217+steventete@users.noreply.github.com>
2025-03-18 12:44:39 -0500

    Wallpaper forbidden fixed

--- src/components/home/Wallpaper.tsx
+++ src/components/home/Wallpaper.tsx
@@ -1,6 +1,6 @@
 const Wallpaper = () => {
   return (
-    <div className="bg-[url('https://w.wallhaven.cc/full/6k/wallhaven-6k3zql.jpg')] h-full w-full rounded-3xl -z-10 bg-cover bg-no-repeat" />
+    <div className="bg-[url('https://i.ibb.co/1YTkX1Xq/wallhaven-6k3zql.jpg')] h-full w-full rounded-3xl -z-10 bg-cover bg-no-repeat" />
   );
 };
 
